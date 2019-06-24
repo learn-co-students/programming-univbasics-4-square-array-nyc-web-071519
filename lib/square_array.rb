6a60804d936db73fb787caf035a8cdd422f27688
tyler knapp <tgknapp11@gmail.com>
2019-06-24 19:48:12 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,20 @@
+# def square_array(array)
+#   l = 0
+#   new_numbers = []
+#
+#   while l < array.length do
+#     new_numbers.push(Integer.sqrt(array[l]))
+#     l += 1
+#   end
+#   return new_numbers
+# end
+
 def square_array(array)
   l = 0
   new_numbers = []
 
   while l < array.length do
-    new_numbers.push(Integer.sqrt(array[l]))
+    new_numbers.push(array[l] ** 2)
     l += 1
   end
   return new_numbers
