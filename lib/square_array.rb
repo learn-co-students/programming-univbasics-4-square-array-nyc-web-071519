9b515a7fba788a20504e13fbcc09bfb71c744662
HyoEun Lee <hyoeun93@gmail.com>
2019-06-30 20:58:41 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,7 @@
 def square_array(array)
-  # your code here
+  counter = 0 
+  while counter < array.length do 
+    var square_array = array[counter]**
+    puts square_array
+    counter += 1
 end
